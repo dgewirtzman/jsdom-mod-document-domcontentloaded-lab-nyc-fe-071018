1e86d51c00efe7b0569f1d12f0f3379597a6a524
David Gewirtzman <dgewirtzman@playbill.com>
2018-08-22 16:01:53 +0000

    automatically backed up by learn

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
-document.addEventListener("DOMContentLoaded", event => {
-  document.querySelector("body").addEventListener("click", e => console.log("Reggae, Reggae!"));
-  }) 
+document.addEventListener("DOMContentLoaded", e => {
+  document.querySelector("body")
+   .addEventListener("click", e => console.log("Reggae, Reggae!"));
+})
